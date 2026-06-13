6121a9ab5f56ff2918cd0d79379fa1dd22fcbbb7
agito0603 <fukuroku0603@yahoo.co.jp>
2026-06-13 11:26:17 +0900

    Improve gold completion mission tab job scanning

--- ICE/Scheduler/Handlers/CosmicHandler.cs
+++ ICE/Scheduler/Handlers/CosmicHandler.cs
@@ -305,7 +305,7 @@ namespace ICE.Utilities
             if (missionManager == null) return 0;
 
             var mission = manager->State.CurrentMission;
-            return mission.Score;
+            return mission.ScoreUInt;
         }
 
     }
